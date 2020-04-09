7432bbc81778bf189a30f80051757a35e083d4ff
StephanGrzelkowski <stephan.grzelkowski@gmail.com>
2020-04-10 00:12:01 +0200

    Added -meta to gitignore; First audio files and spectrum analysis

---
+++ Assets/Scripts/FrequencyAnalyzer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FrequencyAnalyzer : MonoBehaviour
+{
+    public AudioSource cur_sample;
+    public float[] sample = new float[512];
+    // Start is called before the first frame update
+    void Start()
+    {
+        cur_sample = GetComponent<AudioSource>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+    }
+
+    public void GetSpectrumAudioSource()
+    {
+        cur_sample.GetSpectrumData(sample, 0, FFTWindow.Hamming);
+    }
+}
